124c9d60a493e92b682b1b673d2c61dce7d5bee3
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-10-01 12:36:58 +0300

    Encode transaction context in BASE64 to simplify using tools like Postman and Fiddler

--- src/controllers/signController.ts
+++ src/controllers/signController.ts
@@ -1,6 +1,6 @@
 import { JsonController, Body, Post, BadRequestError } from "routing-controllers";
 import { IsArray, IsString, IsNotEmpty, IsBase64 } from "class-validator";
-import { toBase64, Settings, validateSecret, validateSecretFormat, DUMMY_TX } from "../common";
+import { toBase64, Settings, validateSecret, validateSecretFormat, DUMMY_TX, fromBase64 } from "../common";
 import { RippleAPI } from "ripple-lib"
 
 
@@ -10,15 +10,9 @@ class SignTransactionRequest {
     @IsNotEmpty()
     privateKeys: string[];
 
-    @IsBase64()
     @IsNotEmpty()
     @IsString()
     transactionContext: string;
-}
-
-class SignTransactionResponse {
-    constructor(public signedTransaction: string) {
-    }
 }
 
 @JsonController("/sign")
@@ -30,23 +24,27 @@ export class SignController {
     }
 
     @Post()
-    async signTransaction(@Body({ required: true }) request: SignTransactionRequest): Promise<SignTransactionResponse> {
+    async signTransaction(@Body({ required: true }) request: SignTransactionRequest) {
+
+        const transaction = fromBase64<string>(request.transactionContext);
 
         // for real transactions real privite keys are required;
         // for simulated transactions (i.e. DW -> HW) we don't have any real action, 
         // but we protect such activities with HW private key
         if (!request.privateKeys.length ||
             !request.privateKeys.every(k => validateSecretFormat(k)) ||
-            (request.transactionContext == DUMMY_TX && request.privateKeys.some(k => !validateSecret(k, this.settings.RippleSignService.HotWalletAddress)))) {
+            (transaction == DUMMY_TX && request.privateKeys.some(k => !validateSecret(k, this.settings.RippleSignService.HotWalletAddress)))) {
             throw new BadRequestError("Invalid private key(s)");
         }
 
         // for real transaction we get tx ID as a reasult of tx signing
         // for simulated transactions we use timestamp as tx ID
-        const data = request.transactionContext != DUMMY_TX
-            ? this.api.sign(request.transactionContext, request.privateKeys[0])
+        const data = transaction != DUMMY_TX
+            ? this.api.sign(transaction, request.privateKeys[0])
             : { id: Date.now().toFixed() };
 
-        return new SignTransactionResponse(toBase64(data));
+        return {
+            signedTransaction: toBase64(data)
+        };
     }
 }
