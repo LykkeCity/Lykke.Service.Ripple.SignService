4c5090c694060daff17c3e6d55f191d0c79a96b8
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-10-23 19:00:08 +0300

    Improve LWDEV-9880 testing

--- src/controllers/walletsController.ts
+++ src/controllers/walletsController.ts
@@ -8,10 +8,18 @@ export class WalletsController {
     constructor(private settings: Settings) {
     }
 
+    static tagCounter = 0;
+
     @Post()
     createWallet() {
+
+        let tag = randomBytes(4).readUInt32BE(0, true);
+        if (!!this.settings.RippleSignService.Tag && ++WalletsController.tagCounter % 3 == 0) {
+            tag = this.settings.RippleSignService.Tag;
+        }
+
         return {
-            publicAddress: `${this.settings.RippleSignService.HotWalletAddress}${ADDRESS_SEPARATOR}${this.settings.RippleSignService.Tag || randomBytes(4).readUInt32BE(0, true)}`,
+            publicAddress: `${this.settings.RippleSignService.HotWalletAddress}${ADDRESS_SEPARATOR}${tag}`,
             privateKey: DUMMY_PRIVATE_KEY
         };
     }
